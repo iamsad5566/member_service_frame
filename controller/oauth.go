def96de725f0a219846ff6e01972363f53b4c3ab
Yen-Kuang-tw <nf8964p5566@gmail.com>
2024-04-06 17:03:10 +0800

    feat: oauth2.0 developing

--- controller/oauth.go
+++ controller/oauth.go
@@ -13,37 +13,62 @@ import (
 )
 
 func OAuth2Group(server *gin.Engine, userRepo repo.UserRepoInterface, loginTimeRepo repo.LoginTimeInterface) {
-	group := server.Group("/oauth2")
+	groupGoogle := server.Group("/oauth2/google")
 	{
-		group.GET("/login", oauth2LoginHandler)
-		group.GET("/callback", oauth2CallbackHandler)
+		groupGoogle.GET("/register", oauth2RegisterHandler("google"))
+		groupGoogle.GET("/login", oauth2LoginHandler("google"))
+		groupGoogle.GET("/callback", oauth2CallbackHandler("google"))
 	}
 }
 
-var oauth2Config = &oauth2.Config{
-	ClientID:     "325768846939-4s4d9jr5jublsttd2bue1nr2vti8ocib.apps.googleusercontent.com",
-	ClientSecret: "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",
-	RedirectURL:  fmt.Sprintf("https://%s:118/oauth2/callback", config.Setting.GetMemberServiceHost()),
-	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-	Endpoint:     google.Endpoint,
+var oauth2GoogleConfig = &oauth2.Config{
+	ClientID:     config.Setting.GetOauthClientID("google"),
+	ClientSecret: config.Setting.GetOauthClientSecret("google"),
+	RedirectURL: fmt.Sprintf("https://%s%s/oauth2/callback",
+		config.Setting.GetMemberServiceFrameHost(),
+		config.Setting.GetMemberServiceFramePort()),
+	Scopes:   []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint: google.Endpoint,
 }
 
-func oauth2LoginHandler(c *gin.Context) {
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := oauth2Config.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	c.Redirect(http.StatusTemporaryRedirect, url)
-}
-
-func oauth2CallbackHandler(c *gin.Context) {
-	// Use the authorization code that is pushed to the redirect
-	// URL. Exchange will do the handshake to retrieve the initial access token.
-	code := c.Query("code")
-	token, err := oauth2Config.Exchange(context.Background(), code)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Error while exchanging code for token"})
-		return
+func getConfigByProvider(provider string) *oauth2.Config {
+	switch provider {
+	case "google":
+		return oauth2GoogleConfig
 	}
-	// The token now contains the access token
-	c.JSON(http.StatusOK, gin.H{"token": token})
+	return nil
+}
+
+func oauth2RegisterHandler(provider string) gin.HandlerFunc {
+	config := getConfigByProvider(provider)
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func oauth2LoginHandler(provider string) gin.HandlerFunc {
+	config := getConfigByProvider(provider)
+	return func(ctx *gin.Context) {
+		// Redirect user to consent page to ask for permission
+		// for the scopes specified above.
+		url := config.AuthCodeURL("state", oauth2.AccessTypeOffline)
+		ctx.Redirect(http.StatusTemporaryRedirect, url)
+	}
+}
+
+func oauth2CallbackHandler(provider string) gin.HandlerFunc {
+	config := getConfigByProvider(provider)
+	return func(ctx *gin.Context) {
+		// Use the authorization code that is pushed to the redirect
+		// URL. Exchange will do the handshake to retrieve the initial access token.
+		code := ctx.Query("code")
+		token, err := config.Exchange(context.Background(), code)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Error while exchanging code for token"})
+			return
+		}
+		// The token now contains the access token
+		ctx.JSON(http.StatusOK, gin.H{"token": token})
+	}
+
 }
